7ad46783afe9f545f5d98d1abff25e3f1ce0767c
MelvinManni <melvinnnamani@gmail.com>
2020-04-30 22:49:53 +0100

    new commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var share = document.getElementById('fbShare');
+var share = document.getElementById('share');
 let imageSize = document.getElementById('imageSize');
 let clear = document.getElementById('clear');
 let imageNum = 5;
@@ -10,40 +10,6 @@ canvas.set({
   strokeWidth: 10,
   stroke: '#4267b2',
 });
-
-let block = canvas.add(
-  new fabric.Rect({
-    left: 0,
-    top: 0,
-    fill: '#fff',
-    width: 153,
-    height: 20,
-    rx: 2,
-    ry: 2,
-    hasBorders: false,
-    hasControls: false,
-    hasRotatingPoint: false,
-    lockMovementX: true,
-    lockMovementY: true,
-  })
-);
-
-
-let text = canvas.add(
-  new fabric.Text('#CovidFondBox #KuchCorona', {
-    fontFamily: 'Delicious_500',
-    left: 5,
-    top: 5,
-    fontSize: 11,
-    textAlign: 'left',
-    fill: '#4267b2',
-    hasBorders: false,
-    hasControls: false,
-    hasRotatingPoint: false,
-    lockMovementX: true,
-    lockMovementY: true,
-  })
-);
 
 document.getElementById('file').addEventListener(
   'change',
@@ -67,7 +33,7 @@ document.getElementById('file').addEventListener(
             image.scaleToHeight(200);
             image.scaleToWidth(200);
             canvas.add(image);
-            image.sendToBack()
+            image.sendToBack();
             canvas.renderAll();
           });
           img.src = e.target.result;
@@ -80,28 +46,9 @@ document.getElementById('file').addEventListener(
 );
 
 document.getElementById('save').addEventListener('click', function (e) {
-  var canvas = document.getElementById('canvas');
-  canvas.toBlob(function (blob) {
-    saveAs(blob, 'Image.png');
-  });
 
-  if (window.innerWidth <= 800) {
-    var image = document.getElementById('image');
-    var overlayResult = document.getElementById('overlayResult');
-    image.src = canvas.toDataURL('image/jpeg', 0.9);
-    image.width = canvas.width;
-    image.height = canvas.height;
-    overlayResult.className = 'open zoomInUp';
-  }
-});
-
-document.getElementById('back').addEventListener('click', function (e) {
-  var overlayResult = document.getElementById('overlayResult');
-  overlayResult.className = 'close';
-});
-
-clear.addEventListener('click', function () {
-  canvas.remove(...canvas.getObjects());
+  canvas.discardActiveObject();
+  canvas.renderAll();
   let block = canvas.add(
     new fabric.Rect({
       left: 0,
@@ -119,7 +66,6 @@ clear.addEventListener('click', function () {
     })
   );
 
-
   let text = canvas.add(
     new fabric.Text('#CovidFondBox #KuchCorona', {
       fontFamily: 'Delicious_500',
@@ -136,31 +82,31 @@ clear.addEventListener('click', function () {
     })
   );
 
+  setTimeout(() => {
+    document.getElementById('canvas').toBlob(function (blob) {
+      saveAs(blob, 'Image.png');
+    });
 
+    if (window.innerWidth <= 800) {
+      var image = document.getElementById('image');
+      var overlayResult = document.getElementById('overlayResult');
+      image.src = canvas.toDataURL('image/jpeg', 0.9);
+      image.width = canvas.width;
+      image.height = canvas.height;
+      overlayResult.className = 'open zoomInUp';
+    }
+  }, 1000);
 });
 
-share.addEventListener('click', sharefbimage);
+document.getElementById('back').addEventListener('click', function (e) {
+  var overlayResult = document.getElementById('overlayResult');
+  overlayResult.className = 'close';
+});
 
-function sharefbimage() {
-  var canvas = document.getElementById('canvas');
-  image = canvas.toDataURL('image/jpeg', 0.9);
-  FB.init({ appId: `your appid`, status: true, cookie: true });
-  FB.ui(
-    {
-      method: `share`,
-      name: 'Facebook Dialogs',
-      href: $(location).attr('href'),
-      link: 'https://developers.facebook.com/docs/dialogs/',
-      picture: `${image}`,
-      caption: 'Ishelf Book',
-      description: 'your description',
-    },
-    function (response) {
-      if (response && response.post_id) {
-        alert('success');
-      } else {
-        alert('error');
-      }
-    }
-  );
-}
+clear.addEventListener('click', function () {
+  canvas.remove(...canvas.getObjects());
+});
+
+share.addEventListener('click', function () {
+  let src = canvas.toDataURL('image/jpeg', 0.9);
+});
