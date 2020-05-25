23c884044a5222f6c9e32d22be54f56c521f3414
MelvinManni <melvinnnamani@gmail.com>
2020-05-25 14:21:22 +0100

    fixes

--- tabs.js
+++ tabs.js
@@ -18,7 +18,7 @@ function openCity(evt, cityName) {
 var colorList = ['000000', '993300', '333300', '003300', '003366', '000066', '333399', '333333',
     '660000', 'FF6633', '666633', '336633', '336666', '0066FF', '666699', '666666', 'CC3333', 'FF9933', '99CC33', '669966', '66CCCC', '3366FF', '663366', '999999', 'CC66FF', 'FFCC33', 'FFFF66', '99FF66', '99CCCC', '66CCFF', '993366', 'CCCCCC', 'FF99CC'];
 var picker = $('#color-picker');
-
+let div = document.createElement('div')
 for (var i = 0; i < colorList.length; i++) {
     picker.append('<li class="color-item" data-hex="' + '#' + colorList[i] + '" style="background-color:' + '#' + colorList[i] + ';"></li>');
 }
